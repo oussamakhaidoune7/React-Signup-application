0379dbc7556a3ee73d1b26542cef8434134d0d06
Oussama <oussamakhaidoune74@gmail.com>
2024-09-11 17:15:13 +0200

    first commit

---
+++ signup-application/src/Components/LoginSigniup/LoginSignup.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import './Loginsignup.css'
+
+
+
+
+
+const LoginSignup = () => {
+    
+    const [action,setAction] = useState("Login");
+    return (
+        <div className="container">
+            <div className="header">
+                <div className="text">{action}</div>
+                <div className="underline"></div>
+            </div>
+            <div className="inputs">
+                {action==="Login"?<div></div>:<div className="input">
+                    <img src="" alt="" />
+                    <input type="text" placeholder="Full Name" />
+                </div>}
+                
+                <div className="input">
+                    <img src="" alt="" />
+                    <input type="email" placeholder="Email id" />
+                </div>
+                <div className="input">
+                    <img src="" alt="" />
+                    <input type="password" placeholder="Password" />
+                </div>
+            </div>
+            {action==="Sign Up"?<div></div>: <div className="forgot-pasword">Lost Password? <span>Click hier!</span></div>}
+           
+            <div className="submit-container">
+                <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+                <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>login</div>
+
+            </div>
+        </div>
+    );
+};
+
+export default LoginSignup
+
+
